051d962fdf32b3a8a0d3db718f85b699abec09cc
Suicideboy <viauthomas@eisti.eu>
2015-10-30 16:40:39 +0100

    fait en cours

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
-/**************/
-/*** CONFIG ***/
-/**************/
 var PORT = 8080;
 
-
-/*************/
-/*** SETUP ***/
-/*************/
 var express = require('express');
 var http = require('http');
+var sugar = require('sugar');
 var main = express()
 var server = http.createServer(main)
 var io  = require('socket.io').listen(server);
@@ -18,83 +12,70 @@ server.listen(PORT, null, function() {
     console.log("Listening on port " + PORT);
 });
 
-main.get('/', function(req, res){ res.sendFile('client.html'); });
-main.get('/index.html', function(req, res){ res.sendFile('client.html'); });
-main.get('/client.html', function(req, res){ res.sendFile('client.html'); });
-main.get('/adapter.js', function(req,res) { res.sendFile('adapter.js'); });
+var options = {
+    root: __dirname + '/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
 
+main.get('/', function(req, res){ res.sendFile('client.html', options); });
+main.get('/index.html', function(req, res){ res.sendFile('client.html', options); });
+main.get('/client.html', function(req, res){ res.sendFile('client.html', options); });
+main.get('/adapter.js', function(req,res) { res.sendFile('adapter.js', options); });
 
-
-/*************************/
-/*** INTERESTING STUFF ***/
-/*************************/
-var channels = ['Welcome room'];
+var channels = listChannels;
 var sockets = {};
 
-/**
- * Users will connect to the signaling server, after which they'll issue a "join"
- * to join a particular channel. The signaling server keeps track of all sockets
- * who are in a channel, and on join will send out 'addPeer' events to each pair
- * of users in a channel. When clients receive the 'addPeer' even they'll begin
- * setting up an RTCPeerConnection with one another. During this process they'll
- * need to relay ICECandidate information to one another, as well as SessionDescription
- * information. After all of that happens, they'll finally be able to complete
- * the peer connection and will be streaming audio/video between eachother.
- */
 io.sockets.on('connection', function (socket) {
-    socket.channels = {};
+    socket.channel = "Welcome room";
     sockets[socket.id] = socket;
 
     console.log("["+ socket.id + "] connection accepted");
     socket.on('disconnect', function () {
-        for (var channel in socket.channels) {
-            part(channel);
-        }
+        part(socket.channel);
         console.log("["+ socket.id + "] disconnected");
         delete sockets[socket.id];
     });
 
     socket.on('join', function (config) {
         console.log("["+ socket.id + "] join ", config);
-        var channel = config.channel;
+        var channel = decodeURI(config.channel);
         var userdata = config.userdata;
+        if(channels[channel]) {
+            if (channel === socket.channel) {
+                console.log("["+ socket.id + "] ERROR: already joined "+channel);
+                return;
+            } else {
+                part(socket.channel);
+                socket.channel = channel;
+                console.log("["+ socket.id + "] joined '"+channel+"'");
+            }
 
-        if (channel in socket.channels) {
-            console.log("["+ socket.id + "] ERROR: already joined ", channel);
-            return;
-        }
-        for(channel in socket.channels) {
-            console.log("channel : "+channel);
-            part(channel);
-        }
-        if (!(channel in channels)) {
-            channels[channel] = {};
-        }
+            for (id in channels[channel]) {
+                channels[channel][id].emit('addPeer', {'peer_id': socket.id, 'should_create_offer': false});
+                socket.emit('addPeer', {'peer_id': id, 'should_create_offer': true});
+            }
 
-        for (id in channels[channel]) {
-            channels[channel][id].emit('addPeer', {'peer_id': socket.id, 'should_create_offer': false});
-            socket.emit('addPeer', {'peer_id': id, 'should_create_offer': true});
+            channels[channel][socket.id] = socket;
+        } else {
+            console.log("["+ socket.id + "] ERROR: channel '"+ channel+ "' doesn't exist");
         }
-
-        channels[channel][socket.id] = socket;
-        socket.channels[channel] = channel;
     });
 
     function part(channel) {
         console.log("["+ socket.id + "] part '"+channel+"'");
-        if (!(channel in socket.channels)) {
+        console.log(channels[channel]);
+        if (!(channel===socket.channel.id)) {
 
             console.log("["+ socket.id + "] ERROR: not in ", channel);
-            console.log("only in ");
-            for(channel in socket.channels) {
-                console.log(channel+" - ")
-            }
+            console.log("only in "+socket.channel);
             return;
         }
-
-        delete socket.channels[channel];
+        delete socket.channel;
         delete channels[channel][socket.id];
-
         for (id in channels[channel]) {
             channels[channel][id].emit('removePeer', {'peer_id': socket.id});
             socket.emit('removePeer', {'peer_id': id});
